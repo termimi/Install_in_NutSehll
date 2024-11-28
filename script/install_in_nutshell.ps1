30f6b93bbf01d7a884080597cfb5694a6f05b54a
termimi <123395118+termimi@users.noreply.github.com>
2024-11-28 20:31:33 +0100

    [Feat] (install_in_nutshell): Ajout de l'installation de VsCode

--- script/install_in_nutshell.ps1
+++ script/install_in_nutshell.ps1
@@ -1,12 +1,12 @@
+# Parameter help description
+[Parameter]
+[string]
+$InstallPath = $(Get-Location).Path
 function CheckPsVersion {
     return $PSVersionTable.PSVersion
 }
 
 function InstallTeams {
-    param (
-        [Parameter()]
-        [string]$InstallPath = $(Get-Location).Path
-    )
     $downloadLink = "https://go.microsoft.com/fwlink/?linkid=2281613&clcid=0x40c&culture=fr-fr&country=fr"
     try {
         # Téléchargement de l'installer
@@ -24,6 +24,22 @@ function InstallTeams {
         Write-Error "An error has been encoutred: " $_.Exception.Message
     }
 }
+function InstallVsCode {
+    $downloadLink = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/f1a4fb101478ce6ec82fe9627c43efbf9e98c813/VSCodeUserSetup-x64-1.95.3.exe"
+
+    try {
+        Write-Output "Downloading Visual Studio Code installer..."
+        Invoke-WebRequest -Uri $downloadLink -OutFile $InstallPath -UseBasicParsing
+        Write-Output "Visual Studio Code installer downloaded to: $InstallPath"
+        Write-Output "Installing VSCode"
+
+        #TODO: Faire en sorte de ne pas utiliser le nom de l'installer en harde code mais de le récupérer dans un tableau
+        Start-Process -FilePath "$InstallPath\VSCodeUserSetup-x64-1.95.3.exe"
+    }
+    catch {
+        Write-Error "An error has been encoutred: " $_.Exception.Message
+    }
+}
 
 $PowershellVersion = CheckPsVersion
 
@@ -31,3 +47,4 @@ if (($PowershellVersion.Major -lt 5) -or ($PowershellVersion.Major -lt 7) ) {
     Write-Error "Your version of powershell is to old to execute this script"
 }
 InstallTeams
+InstallVsCode
