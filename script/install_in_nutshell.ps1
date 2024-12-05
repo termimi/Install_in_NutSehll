6a963ee2ee80d049ba8f7cbaa0ba6f3ef9c67139
termimi <123395118+termimi@users.noreply.github.com>
2024-12-05 13:25:57 +0100

    [feat]: Installation de docker

--- script/install_in_nutshell.ps1
+++ script/install_in_nutshell.ps1
@@ -21,7 +21,7 @@ function InstallTeams {
         #Remove-Item -Path "$InstallPath\MSTeamsSetup.exe" -Force
     }
     catch {
-        Write-Error "An error has been encoutred: " $_.Exception.Message
+        Write-Error "An error has been encountered: " + $_.Exception.Message
     }
 }
 function InstallVsCode {
@@ -32,19 +32,65 @@ function InstallVsCode {
         Invoke-WebRequest -Uri $downloadLink -OutFile $InstallPath -UseBasicParsing
         Write-Output "Visual Studio Code installer downloaded to: $InstallPath"
         Write-Output "Installing VSCode"
-
         #TODO: Faire en sorte de ne pas utiliser le nom de l'installer en harde code mais de le récupérer dans un tableau
         Start-Process -FilePath "$InstallPath\VSCodeUserSetup-x64-1.95.3.exe"
     }
     catch {
-        Write-Error "An error has been encoutred: " $_.Exception.Message
+        Write-Error "An error has been encountered: " + $_.Exception.Message
+    }
+}
+function InstallDocker {
+    $downloadLink = "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module&_gl=1*1mq0u4u*_gcl_au*NjY4NTY5NTgwLjE3MzMzODI3NDg.*_ga*MTgyMDcxMzA3MC4xNzMzMzgyNzQ4*_ga_XJWPQMJYHQ*MTczMzM4Mjc0OC4xLjEuMTczMzM4Mjc3NS4zMy4wLjA."
+
+    try {
+        Write-Output "Downloading docker desktop installer..."
+        Invoke-WebRequest -Uri $downloadLink -OutFile $InstallPath -UseBasicParsing
+        Write-Output "docker desktop installer downloaded to: $InstallPath"
+        Write-Output "Installing docker desktop"
+
+        #TODO: Faire en sorte de ne pas utiliser le nom de l'installer en harde code mais de le récupérer dans un tableau
+        Start-Process -FilePath "$InstallPath\Docker Desktop Installer.exe"
+    }
+    catch {
+        Write-Error "An error has been encountered: " + $_.Exception.Message
+    }
+}
+function CheckForDocker {
+    $isDockergroupHere = Get-LocalGroup | Where-Object {$_.Name -like "*docker*"}
+    if($null -ne $isDockergroupHere){
+        return $true
+    }
+    else{
+        return $false
     }
 }
 
 $PowershellVersion = CheckPsVersion
+$currentuser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principalUser = New-Object System.Security.Principal.WindowsPrincipal($currentuser)
 
 if (($PowershellVersion.Major -lt 5) -or ($PowershellVersion.Major -lt 7) ) {
     Write-Error "Your version of powershell is to old to execute this script"
 }
 InstallTeams
 InstallVsCode
+if ($principalUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    try {
+        $isDockerHere = CheckForDocker
+        if($isDockerHere){
+            $currentuser = $currentuser.Name
+            $username = $currentUser.Split('\')[-1]
+            net localgroup docker-users $username /ADD
+            Write-Output "Adding user to docker group"
+        }
+        else {
+            InstallDocker
+        }
+    }
+    catch {
+        Write-Error ("An error has been encountered: " + $_.Exception.Message)
+    }
+}
+else {
+    Write-Output "Script not used with administrator rights - docker cannot be installed or used"
+}
